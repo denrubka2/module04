7996bed0c5d8725748e6862e73f9f0b561263efc
Denis <denrubka2@mail.ru>
2024-06-22 19:51:17 +0700

    fix

--- assets/js/taskHard_1.js
+++ assets/js/taskHard_1.js
@@ -3,9 +3,9 @@
 const earnings = +prompt('Введите сумму заработка');
 
 if (earnings < 15000) {
-  console.log(`Ваш налог составляет ${ earnings * 0.13 }`);
+  console.log(`Ваш налог составляет ${ Math.round(earnings * 0.13) }`);
 } else if (earnings < 50000) {
-  console.log(`Ваш налог составляет ${ earnings * 0.20 }`);
+  console.log(`Ваш налог составляет ${ Math.round(earnings * 0.20) }`);
 } else {
-  console.log(`Ваш налог составляет ${ earnings * 0.30 }`);
+  console.log(`Ваш налог составляет ${ Math.round(earnings * 0.30) }`);
 }
--- assets/js/taskHard_2.js
+++ assets/js/taskHard_2.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const earnings = +prompt('Введите сумму заработка');
+const income = +prompt('Введите сумму заработка');
 
-if (earnings < 15000) {
-  console.log(`Ваш налог составляет ${ earnings * 0.13 }`);
-} else if (earnings <= 50000) {
-  console.log(`Ваш налог составляет ${ ( earnings - 15000 ) * 0.20 }`);
+if (income < 15000) {
+  console.log(`Ваш налог составляет ${ Math.round( income * 0.13 ) }`);
+} else if (income <= 50000) {
+  console.log(`Ваш налог составляет ${ Math.round( ( income - 15000 ) * 0.20 ) }`);
 } else {
-  console.log(`Ваш налог составляет ${ ( earnings - 50000 ) * 0.30 }`);
+  console.log(`Ваш налог составляет ${ Math.round( ( income - 50000 ) * 0.30 ) }`);
 }
