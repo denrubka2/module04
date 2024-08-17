621a9926f267cb7e1db5d99c5b900e2857de489a
Denis <denrubka2@mail.ru>
2024-08-17 14:13:52 +0700

    09

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -2,31 +2,40 @@
 
 const cart = {
   items: [],
-  totalPrice: 0,
+  _totalPrice: 0,
   count: 0,
-  getTotalPrice() {
-    return this.totalPrice;
+
+  get totalPrice() {
+    return this.calculateItemPrice();
   },
+
+  set totalPrice(value) {
+    this._totalPrice = value;
+  },
+
   add(productTitle, productPrice, productQuantity = 1) {
     this.items.push({ productTitle, productPrice, productQuantity });
-    this.calculateItemPrice();
     this.increaseCount(productQuantity);
   },
+
   increaseCount(number = 1) {
     this.count += number;
   },
+
   calculateItemPrice() {
-    this.totalPrice = this.items.reduce((acc, current) => acc + current['productPrice'], 0 );
+    return this.items.reduce((acc, current) => acc + current['productPrice'], 0 );
   },
+
   clear() {
     this.items = [];
     this.totalPrice = 0;
     this.count = 0;
   },
+
   print() {
     console.log(`Все товары: ${JSON.stringify(this.items)}`);
-    console.log(`Общая стоимость товаров: ${this.getTotalPrice()} спичек`);
-  }
+    console.log(`Общая стоимость товаров: } спичек`);
+  },
 }
 
 cart.add('Носки', 1000, 2);
