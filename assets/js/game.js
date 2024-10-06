cbe3667f10e8b4dcb15aafe2b12218967bf0a22e
denrubka2 <denrubka@mail.ru>
2024-10-06 14:33:15 +0700

    fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,23 +1,23 @@
-// 'use strict';
+'use strict';
 
-// (() => {
-//   const FIGURES_ENG = ['Rock', 'Scissors', 'Paper'];
-//   const FIGURES_RUS = ['Камень', 'Ножницы', 'Бумага'];
+(() => {
+  const FIGURES_ENG = ['Rock', 'Scissors', 'Paper'];
+  const FIGURES_RUS = ['Камень', 'Ножницы', 'Бумага'];
 
-//   const getRandomIntInclusive = (min, max) => {
-//     const minCeiled = Math.ceil(min);
-//     const maxFloored = Math.floor(max);
+  const getRandomIntInclusive = (min, max) => {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
 
-//     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
-//   }
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+  }
 
-//   const getFigure = () => {
-//     const userFigure = prompt(`${FIGURES_RUS.slice(',')}?`);
-//     const pcFigure = FIGURES_RUS[getRandomIntInclusive(0, FIGURES_RUS.length - 1)];
+  const getFigure = () => {
+    const userFigure = prompt(`${FIGURES_RUS.slice(',')}?`);
+    const pcFigure = FIGURES_RUS[getRandomIntInclusive(0, FIGURES_RUS.length - 1)];
 
 
-//   }
+  }
 
-//   getFigure();
+  getFigure();
 
-// })();
+})();
