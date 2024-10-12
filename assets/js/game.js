541c3208d8a119aef84167139610026ccb423973
denrubka2 <denrubka@mail.ru>
2024-10-12 14:50:21 +0700

    add game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,95 +1,49 @@
 'use strict';
 
 (() => {
-  const FIGURES = {
-    ENG: ['Rock', 'Scissors', 'Paper'],
-    RUS: ['Камень', 'Ножницы', 'Бумага'],
-  }
-
-  const getLanguageFigures = (lang) => {
-    if(lang) {
-      return Object.keys(FIGURES).find(str => str.toLowerCase().startsWith(lang.toLowerCase()));
-    }
-  }
-
-  const getRandomIntInclusive = (min, max) => {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
-  }
-
   const results = {
-    user: 0,
-    computer: 0,
+    userBalls: 5,
+    computerBalls: 5,
   }
 
-  const game = (language = '') => {
-    const lang = getLanguageFigures(language) ? getLanguageFigures(language) : 'RUS';
-    const figures = FIGURES[lang];
+  const game = () => {
 
-    const getResults = () => alert(`Счет :\n` +
-      `Вы : ${results.user}\n` +
-        `Компьютер : ${results.computer}`);
-
-    const getUserFigure = (userChoice) => 
-      figures.find(str => str.toLowerCase().startsWith(userChoice.toLowerCase()));
-
-    const getComputerFigure = () => 
-      figures[getRandomIntInclusive(0, figures.length - 1)];
+    const getResults = () => alert(`Счет :\n
+      Вы : ${results.userBalls}\n
+      Компьютер : ${results.computerBalls}`);
 
     return function start() {
-      const word = prompt(`${figures} ?`);
 
-      if(word === null) {
-        if(confirm('Вы точно хотите выйти?')) {
-          alert('Игра окончена...');
-          getResults();
-          return false;
-        } else {
-          return start();
-        }
-      }
+      const rules = ['Четное', 'Нечетное'];
+      const userEnter = prompt(`Загадай число шариков от 1 до ${results.userBalls}`);
+      const computerEnter = rules[Math.round(Math.random() * 1)];
 
-      if(word.trim() === '') {
-        return start();
-      }
-    
-      const userFigure = getUserFigure(word);
-      const userIndex = figures.indexOf(userFigure);
-      const computerFigure = getComputerFigure();
-      const computerIndex = figures.indexOf(computerFigure);
-      
       switch (true) {
-        case userFigure === undefined:
+        case userEnter === null:
+          return false;
+        case userEnter.trim() === '':
+        case userEnter < 1:
+        case userEnter > results.userBalls:
           return start();
-        case userFigure === computerFigure:
-          alert(`Вы : ${userFigure}\n` + 
-            `Компьютер : ${computerFigure}\n` + 
-              'Ничья!');
+        case rules[userEnter % 2] === computerEnter:
+          alert(`Вы : ${userEnter} \n
+            Компьютер : ${computerEnter}\n
+            Компьютер выиграл!\n
+            Количество ваших шариков : ${results.userBalls}\n
+            Количество шариков у компьютера : ${results.computerBalls}`);
           break;
-        case (userIndex + 1) % figures.length === computerIndex:
-          alert(`Вы : ${userFigure}\n` + 
-            `Компьютер : ${computerFigure}\n` + 
-              'Вы победили!');
-          ++results.user;
-        break;
-        default:
-          alert(`Вы : ${userFigure}\n` + 
-            `Компьютер : ${computerFigure}\n` + 
-              'Вы проиграли!');
-          ++results.computer;
-      }
-
-      if(confirm('Играем еще?')) {
-        return start();
-      } else {
-        getResults();
+        case rules[userEnter % 2] !== computerEnter:
+          alert(`Вы : ${userEnter} \n
+            Компьютер : ${computerEnter}\n
+            Компьютер проиграл!\n
+            Количество ваших шариков : ${results.userBalls}\n
+            Количество шариков у компьютера : ${results.computerBalls}`);
+          break;
       }
     }
+  }
 
-    }
-
-    window.RPS = game('EN');
+  const startGame = game();
+  startGame()
 
 })();
